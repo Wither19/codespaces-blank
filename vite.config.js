d2dcd83e4f16b5c8c8be4683b7ce81c7424a9b18
Wither19 <josephvranicar1@gmail.com>
2025-02-14 10:08:11 -0500

    c

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	root: path.resolve(__dirname, "src"),
+	resolve: {
+		alias: {
+			"~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+		},
+	},
+	server: {
+		port: 8080,
+		hot: true,
+	},
+});
