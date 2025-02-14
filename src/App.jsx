2853f4e3212d38791f0cc4d10019e762f95f7bbd
Wither19 <josephvranicar1@gmail.com>
2025-02-14 09:57:34 -0500

    c

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,13 @@ import PokemonMenu from "./components/PokemonMenu";
 
 import { useContext, useEffect, useState } from "react";
 
-import "@fontsource/ubuntu-sans/400.css";
-import "@fontsource/ubuntu-sans/400-italic.css";
-import "@fontsource/ubuntu-sans/700.css";
-import "@fontsource/ubuntu-sans/700-italic.css";
-
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/400-italic.css";
-import "@fontsource/roboto/700.css";
-import "@fontsource/roboto/700-italic.css";
-
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/400-italic.css";
-import "@fontsource/inter/700.css";
-import "@fontsource/inter/700-italic.css";
-
 function App() {
 	return (
 		<>
 			<div className="container-md">
 				<div className="row">
 					<div className="col-4"></div>
-					<div className="col-4">
+					<div className="col-4 d-flex justify-content-center">
 						<h1 className="pokedex-title">Pokédex</h1>
 					</div>
 					<div className="col-4"></div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import bootstrap from "bootstrap";
+
+import "@fontsource/ubuntu-sans/400.css";
+import "@fontsource/ubuntu-sans/400-italic.css";
+import "@fontsource/ubuntu-sans/700.css";
+import "@fontsource/ubuntu-sans/700-italic.css";
+
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/400-italic.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/700-italic.css";
+
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/400-italic.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/700-italic.css";
+
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
